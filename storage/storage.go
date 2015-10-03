14fc8ed8f78fb5e354a174b921092ec20cf7bd0e
er5 <er5@sogetthis.com>
2015-10-02 23:06:09 -0300

    use expiration date in the storage

--- storage/storage.go
+++ storage/storage.go
@@ -2,16 +2,27 @@ package storage
 
 import (
     "github.com/kv/kv/core"
+    "time"
 )
 
 type MapStorage map[string]core.Value
 
 func (m MapStorage) Get(k string) (core.Value, bool) {
     v, ok := m[k]
+
+    if v.ExpirationTime < int(time.Now().Unix()) {
+       delete(m, k)
+       return core.Value{}, false
+    }
+
     return v, ok
 }
 
 func (m MapStorage) Set(k string, flags int, expirationTime int, data []byte) {
+    if expirationTime <= 60 * 60 * 24 * 30 {
+       expirationTime += int(time.Now().Unix())
+    }
+
     m[k] = core.Value{
     	Flags: flags,
     	ExpirationTime: expirationTime,
