e110d1a991713c3076b2f8da7c53b17f54e7c5b0
Marco <marcojetson@gmail.com>
2015-10-05 20:23:46 +0200

    add error messages

---
+++ server/strings.go
@@ -0,0 +1,9 @@
+package server
+
+const response_error = "ERROR"
+const response_error_no_index = "ERROR_NO_INDEX"
+
+const response_queued = "QUEUED"
+
+const response_success = "OK"
+const response_success_arg = "OK %s"
